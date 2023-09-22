a6a5b5afcb6a073514402951585e49563add373d
mafeermoreno <marifermg29@gmail.com>
2023-09-21 20:41:12 -0600

    Agregar comentarios

--- Actividad_Hash/main.cpp
+++ Actividad_Hash/main.cpp
@@ -1,3 +1,18 @@
+// ===============================================================================================================
+// File: main.cpp
+// Date: September 21, 2023
+// Author: María Fernanda Moreno Gómez A01708653
+//         Uri Jared Gopar Morales A01709413
+//         Ricardo Rosales Castañeda A01709449
+// Description: El programa recibe el nombre de un archivo.txt seguido de n que es múltiplo de 4, es mayor o 
+//              igual a 16 pero menor o igual a 64, n determina el número de columnas que tendrá la tabla para 
+//              acomodar absolutamente todos los caracteres. Si el número de caracteres no es múltiplo de N, el 
+//              último renglón de la tabla se rellena con el valor de n. De salida se da la concatenación de la
+//              representación hexadecimal de la posición del arreglo.
+// To compile: g++ main.cpp -o app y después ./app
+// Complexity: La complejidad de todo el programa es o(N), ya que dependen los ciclos totalmente del tamaño de N
+// ==============================================================================================================
+
 #include <iostream>
 #include <fstream>
 #include <iomanip>
@@ -26,6 +41,8 @@ int main() {
     vector<vector<char>> tabla;
     char ch;
     vector<char> fila;
+    //Lee el archivo caracter por caracter y guarda los datos en el vector fila, hasta que fila sea del mismo tamaño que "n", por lo que 
+    //la complejidad algorítmica aquí es O(n)
     while (inFile.get(ch)) {
         fila.push_back(ch);
         if (fila.size() == n) {
@@ -43,6 +60,7 @@ int main() {
     }
 
     cout << "\nTabla:\n";
+    //Creación de la tabla, donde cada fila tiene n caracteres. Complejidad de O(n) al depender de n en el ciclo
     for (auto& fila : tabla) {
         for (char c : fila) {
             if (c == '\n') {
@@ -54,6 +72,7 @@ int main() {
         cout << endl;
     }
     vector<int> a(n, 0);
+    //Recorre cada columna y suma los valores de los caracteres de esa columna en filas. 
     for (int col = 0; col < n; ++col) {
         for (int row = 0; row < tabla.size(); ++row) {
             a[col] += static_cast<int>(tabla[row][col]);
@@ -74,7 +93,7 @@ int main() {
     }
     salida = ss.str();
 
-    cout << "\nSalida:\n";
+    cout << "\nRepresentacion hexadecimal:\n";
     for (int i = 0; i < n; i++) {
         cout << salida.substr(i*2, 2);
         if ((i+1) % 4 == 0 && i != 2*n - 1) { 
