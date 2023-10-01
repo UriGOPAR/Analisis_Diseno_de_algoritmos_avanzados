5982afa28d192614d1562f0b156e00efbfc87f16
mafeermoreno <marifermg29@gmail.com>
2023-10-01 12:22:22 -0600

    Add comments

---
+++ ActInt1_Equipo_05/Equipo_05_main/main.cpp
@@ -0,0 +1,12 @@
+// ================================================================================================================
+// File: main.cpp
+// Date: September 24, 2023
+// Author: María Fernanda Moreno Gómez A01708653
+//         Uri Jared Gopar Morales A01709413
+//         Ricardo Rosales Castañeda A01709449
+// Description: El programa recibe una cadena de máximo 12 caracteres, donde  se sacan sus sufijos y posteriormente
+//              estos sufijos son ordenados con el ordenamiento radix para poder calcular el arreglo de substrings
+// To compile: g++ main.cpp -o app y después ./app
+// Complexity: 
+// ================================================================================================================
+
