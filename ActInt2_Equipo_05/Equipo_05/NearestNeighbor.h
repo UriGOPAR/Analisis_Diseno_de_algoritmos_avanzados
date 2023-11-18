c2de6d84b6b4a999033f7660d56b4162102601bf
rico020202 <ricoroca2@gmail.com>
2023-11-18 15:10:38 -0600

    add documentation

--- ActInt2_Equipo_05/Equipo_05/NearestNeighbor.h
+++ ActInt2_Equipo_05/Equipo_05/NearestNeighbor.h
@@ -1,6 +1,20 @@
+// ===========================================================================
+// File: NearestNeighbor.h
+// Date: November 13, 2023
+// Author: María Fernanda Moreno Gómez A01708653
+//         Uri Jared Gopar Morales A01709413
+//         Ricardo Rosales Castañeda A01709449
+// Description: This file contains the implementation of the Nearest Neighbor
+//              algorithm.
+// Complexity: O(n^2)
+// ===========================================================================
+
 #ifndef NEARESTNEIGHBOR_H
 #define NEARESTNEIGHBOR_H
 
+// ===========================================================================
+// libraries
+// ===========================================================================
 #include <iostream>
 #include <vector>
 #include <fstream>
@@ -9,7 +23,14 @@
 
 using namespace std;
 
-// Función para encontrar la solución al TSP utilizando el Vecino más Cercano
+// ===========================================================================
+// Function: nearestNeighbor
+// Description: This function finds the shortest path that visits each vertex
+//              exactly once and returns to the starting vertex.
+// Parameters: vector<vector<int>>& graph: adjacency matrix of the graph.
+// Return value: vector<int> tour: vector with the order of the vertices.
+// Complexity: O(n^2)
+// ===========================================================================
 vector<int> nearestNeighbor(const vector<vector<int>>& graph) {
     int n = graph.size();
     vector<int> tour;
@@ -38,12 +59,18 @@ vector<int> nearestNeighbor(const vector<vector<int>>& graph) {
         current = next;
     }
 
-    // Asegúrate de que el tour regrese al primer nodo
     tour.push_back(tour[0]);
 
     return tour;
 }
 
+// ===========================================================================
+// Function: printTour
+// Description: This function prints the tour and its cost.
+// Parameters: vector<vector<int>>& graph: adjacency matrix of the graph.
+// Return value: none.
+// Complexity: O(n)
+// ===========================================================================
 void printTour(const vector<vector<int>>& graph) {
     int N = graph.size();
     vector<int> tour = nearestNeighbor(graph);
@@ -57,7 +84,6 @@ void printTour(const vector<vector<int>>& graph) {
     cout << "El costo: ";
     double totalDistance = 0;
 
-    // Calcular la distancia total según los valores de la matriz de adyacencia
     for (int i = 0; i < N; ++i) {
         int node1 = tour[i];
         int node2 = tour[i + 1];
